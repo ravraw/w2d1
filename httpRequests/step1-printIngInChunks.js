4150c2b43d648c026c1b9ebb8a0dbff0a15ddf2b
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-29 06:58:15 -0600

    step1 - printingInChunks - data fetched and logged in chunks

--- httpRequests/step1-printIngInChunks.js
+++ httpRequests/step1-printIngInChunks.js
@@ -12,13 +12,25 @@ const getAndPtrintHTMLChunks = () => {
   https.get(requestOptions, res => {
     // set response encoding to UTF8
     res.setEncoding('UTF8');
+
     // output data recieved
     res.on('data', data => {
       console.log(`${data} \n`);
     });
+
+    // log error
+    res.on('error', err => {
+      console.error(err.stack);
+    });
+
     //log message to completion of response
     res.on('end', () => {
-      console.log('Successfuly read the data');
+      console.log('No more data read the data');
+    });
+
+    // finish
+    res.on('finish', () => {
+      console.log('All data logged to stdOut');
     });
   });
 };
