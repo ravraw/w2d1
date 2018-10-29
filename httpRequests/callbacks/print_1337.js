f04a606dd723b87b5e1c8f520d126c2f096ba84b
ravindra rawat <ravinrawat.wd@gmail.com>
2018-10-29 14:26:44 -0600

    initiated project , added request package and .gitignore

--- httpRequests/callbacks/print_1337.js
+++ httpRequests/callbacks/print_1337.js
@@ -1,2 +1,24 @@
 const https = require('https');
-const helperFunction = require('../httpRequests/http-functions');
+const helperFunctions = require('../http-functions');
+
+const print1337 = dataString => {
+  let code = {
+    a: '4',
+    e: '3',
+    l: '1',
+    o: '0',
+    s: '5',
+    t: '7',
+    ck: 'x',
+    er: '0r',
+    you: 'j00'
+  };
+  console.log(dataString);
+};
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+helperFunctions.getHTML(requestOptions, print1337);
--- httpRequests/callbacks/print_reverse.js
+++ httpRequests/callbacks/print_reverse.js
@@ -1,2 +1,18 @@
 const https = require('https');
-const helperFunction = require('../httpRequests/http-functions');
+const helperFunctions = require('../http-functions');
+
+const printReverse = dataString => {
+  console.log(
+    dataString
+      .split('')
+      .reverse()
+      .join('')
+  );
+};
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+helperFunctions.getHTML(requestOptions, printReverse);
